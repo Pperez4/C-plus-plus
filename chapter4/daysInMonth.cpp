c500e443aaab26645e7185abbfc9370b656a6209
Paola Perez <94589669+Pperez4@users.noreply.github.com>
2023-06-21 14:53:18 -0500

    Adding

--- chapter4/daysInMonth.cpp
+++ chapter4/daysInMonth.cpp
@@ -6,11 +6,12 @@ and the year. The program should then display the number
 of days in that month. Use the following criteria to identify
 leap years:
 1. Determine whether the year is divisible by 100. If it is, 
-then it is a leap year if and only if it is divisible by 400. F
-or example, 2000 is a leap year but 2100 is not.
+then it is a leap year if and only if it is divisible by 400. 
+For example, 2000 is a leap year but 2100 is not.
 2. If the year is not divisible by 100, then it is a leap year
 if and if only it is divisible by 4. For example, 2008 is a leap
 year but 2009 is not.
+
 -----------------------------------------------------------------*/
 
 //------------------------------
@@ -26,5 +27,109 @@ using namespace std;
 //------------------------------
 int main()
 {
-    
+
+    //Variable for year
+    int year;
+    //Variable for month in the range 1-12
+    int month;
+    //For calculation
+    int remainder;
+
+    cout << endl;
+    cout << "Enter a month in the range 1-12\n"
+         << "for example, if you wish to enter\n"
+         << "August simple type 8 and press [ENTER].\n"
+         << "Enter month: ";
+    cin >> month;
+    cout << "Enter a year: ";
+    cin >> year;
+
+cout << endl;
+cout << "Year: " << year << "\n";
+cout << "Month: " << month << "\n";
+
+if (year > 0 && month > 0 && month <= 12)
+{
+            switch (month)
+        {
+        case 1:
+                cout << "----------\n";
+                cout << "31 days\n";
+                cout << "----------\n";
+            break;
+        case 2:
+                remainder = year % 4;
+                if (remainder == 0)
+                {
+                    cout << "----------\n";
+                    cout << "29 days\n";
+                    cout << "----------\n";
+                }
+                else if (remainder > 0)
+                {
+                    cout << "----------\n";
+                    cout << "28 days\n";
+                    cout << "----------\n";
+                }
+            break;
+        case 3: 
+                cout << "----------\n";
+                cout << "31 days\n";
+                cout << "----------\n";
+        case 4:
+                cout << "----------\n";
+                cout << "30 days\n";
+                cout << "----------\n";
+
+            break;
+        case 5:
+                cout << "----------\n";
+                cout << "31 days\n";
+                cout << "----------\n";
+            break;
+        case 6: 
+                cout << "----------\n";
+                cout << "30 days\n";
+                cout << "----------\n";
+            break;
+        case 7:
+                cout << "----------\n";
+                cout << "31 days\n";
+                cout << "----------\n";
+            break;
+        case 8:
+                cout << "----------\n";
+                cout << "31 days\n";
+                cout << "----------\n";
+            break;
+        case 9:
+                cout << "----------\n";
+                cout << "30 days\n";
+                cout << "----------\n";
+            break;
+        case 10:
+                cout << "----------\n";
+                cout << "31 days\n";
+                cout << "----------\n";
+            break;
+        case 11:
+                cout << "----------\n";
+                cout << "30 days\n";
+                cout << "----------\n";
+            break;
+        case 12:
+                cout << "----------\n";
+                cout << "31 days\n";
+                cout << "----------\n";
+            break;
+        default:
+            break;
+}
+
+}
+else
+    cout << "Either the value for year or month is not valid.\n";
+
+
+
 }
--- chapter4/longDistanceCalls.cpp
+++ chapter4/longDistanceCalls.cpp
@@ -34,6 +34,65 @@ using namespace std;
 //------------------------------
 int main()
 {
+    
+    //To promt user
+    double callStartingTime;
+    int minutesCallDuration;
 
+    //For calculation
+    double ratePerMinute; 
+    double total;
+
+// The program should ask for the timeto be entered as a floating point 
+//number in the form HH.MM. 
+
+cout << "---------------------------------------------------------------------------\n";
+cout << "\t\tInstructions\n";
+cout << "Enter the starting time of the call in HH.MM format.\n"
+     << "for example if the call started at 07:00 hours simply\n"
+     << "enter 07.00\n";
+cout << "NOTE The program does not accept times that are greater than 23:59\n" 
+     << "Also, no number whose last two digits are greater than 59 are accepted.\n";
+cout << "Starting Time: ";
+cin >> callStartingTime;
+cout << "Enter the duraration of the call in minutes: ";;
+cin >> minutesCallDuration;
+    
+
+if (callStartingTime  -  static_cast<int>(callStartingTime) <= .59 && callStartingTime < 23.59)
+{
+    cout << "Starting Time: " << callStartingTime << endl;
+    cout << "Minutes: " << minutesCallDuration << endl;
+    cout << fixed << showpoint << setprecision(2);
+    // Starting time 00:00–06:59  rate per minute 0.05
+    if (callStartingTime > 00.00 && callStartingTime <= 06.59)
+    {
+        ratePerMinute = 0.05;
+        total = ratePerMinute * minutesCallDuration;
+        cout << "-----------------------------------\n";
+        cout << "Total charges: $" << total << endl;
+        cout << "-----------------------------------\n";
+    }
+    else if (callStartingTime >= 07.00 && callStartingTime <= 19.00)
+    {
+         // Starting time 07:00–19:00  rate per minute 0.45
+         ratePerMinute = 0.45;
+         total = ratePerMinute * minutesCallDuration;
+         cout << "-----------------------------------\n";
+         cout << "Total charges: $" << total << endl;
+         cout << "-----------------------------------\n";
+    }
+    else if (callStartingTime >= 19.01 && callStartingTime <= 23.59)
+    {
+         // Starting time 19:01 - 23.59  rate per minute 0.20
+         ratePerMinute = 0.20;
+         total = ratePerMinute * minutesCallDuration;
+         cout << "-----------------------------------\n";
+         cout << "Total charges: $" << total << endl;
+         cout << "-----------------------------------\n";
+    }
+}
+else if (callStartingTime  -  static_cast<int>(callStartingTime) > .59 || callStartingTime > 23.59)
+    cout << "Invalid value for starting time.\n";
 
 }
